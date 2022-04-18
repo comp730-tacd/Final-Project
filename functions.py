e9cd87dd1a97bb6e4799d1fd69141bb93311544e
DNicholas-GIT <dan1024@wildcats.unh.edu>
2022-04-18 13:27:21 -0400

    Copied Tonies Files

---
+++ functions.py
@@ -0,0 +1,13 @@
+import locations
+
+class Vehicle_Commands:
+    def gas_capacity(self, capacity, cur_amount):
+        if(0 < cur_amount < capacity):
+            state = "good"
+        elif(cur_amount < 0):
+            state = "Gas tank Empty!"
+        elif(cur_amount > capacity):
+            state = "Gas tank Overfilled!"
+        else:
+            state = "error"
+        return state
