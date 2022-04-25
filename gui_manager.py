8c073e131a3a8177c79bb756a2f9d4ac48c8ae0b
DNicholas-GIT <dan1024@wildcats.unh.edu>
2022-04-25 18:44:19 -0400

    Fixed GUI

--- gui_manager.py
+++ gui_manager.py
@@ -1,23 +1,22 @@
 from tkinter import *
-  
+
 root = Tk()
  
 
 # Adjust size 
-root.geometry("1920x1200")
+root.geometry("800x470")
   
 # Add image file
 bg = PhotoImage(file = "assets/house.png")
   
 # Create Canvas
-location_image = Canvas( root, width = 1000, height = 800)
-top_menu = Canvas(root, width = 1000, height = 200 )  
+location_image = Canvas( root, width = 800, height = 600)
+top_menu = Canvas(root, width = 800, height = 100, background="Black" )  
 
-  
+
 # Display image
-location_image.create_image( 0, 0, image = bg)
-top_menu.create_rectangle(0, 0, 100, 100)
-  
+location_image.create_image( 0, 0, image = bg, anchor=NW)
+
 
 # functions
 def click_travel():
@@ -30,17 +29,23 @@ def click_load():
     pass
 
 # Create Buttons
-travel_button = Button(root, text="Travel", padx=50, command=click_travel,fg="white", bg="#929493")
-save_button = Button(root, text="Save", padx=50, command=click_save,fg="white", bg="#929493")
-load_button = Button(root, text="Load", padx=50, command=click_load,fg="white", bg="#929493")
+travel_button = Button(root, text="Travel", padx=50, pady=10, command=click_travel,fg="white", bg="#929493")
+save_button = Button(root, text="Save", padx=50, pady=10, command=click_save,fg="white", bg="#929493")
+load_button = Button(root, text="Load", padx=50, pady=10, command=click_load,fg="white", bg="#929493")
 
 # display
 # travel_button.grid(row=0, column=0)
 # save_button.grid(row=0, column=1)
 # load_button.grid(row=0, column=2)
 
-top_menu.grid(row=1, column=0)
-location_image.grid(row=2, column=0)
+button1_canvas = top_menu.create_window( 10, 30, anchor = "nw", window = travel_button)
+  
+button2_canvas = top_menu.create_window( 190, 30, anchor = "nw", window = save_button)
+  
+button3_canvas = top_menu.create_window( 370, 30, anchor = "nw", window = load_button)
+
+top_menu.grid(row=0, column=0)
+location_image.grid(row=1, column=0)
 
 
 # Execute tkinter
